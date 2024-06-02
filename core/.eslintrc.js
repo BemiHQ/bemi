129219795a5ba23cdb6ca9fdb4dd1f3dc3f92588
haecheonlee <dlgocjs1@gmail.com>
2024-06-02 01:53:51 -0400

    fea: integrate eslint and prettier

---
+++ core/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+};
+  
