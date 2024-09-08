0819b299bf0122aff86a183040981bdebfb6b7ef
Arjun Lall <lallarjun@gmail.com>
2024-09-08 16:55:33 -0700

    Add tracking

--- docs/src/js/clientModule.js
+++ docs/src/js/clientModule.js
@@ -24,6 +24,9 @@ if (ExecutionEnvironment.canUseDOM) {
       })
     }
   })(document,"script");
+
+  // Koala tracking
+  !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_e3caa97254c50b34a600ed2277dd29336c65/sdk.js"),(document.body || document.head).appendChild(n)}();  
 }
 
 export function onRouteDidUpdate() {
