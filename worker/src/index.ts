28c3819f8893927a31c8a47f91fa9d7737b4e2c2
Sriram-52 <sriram.nyshadham@ateliasoftware.com>
2024-11-11 14:12:40 -0500

    Update bemi configuration

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,13 +1,13 @@
-import { AckPolicy, DeliverPolicy } from 'nats';
-import { MikroORM } from '@mikro-orm/postgresql';
+import { AckPolicy, DeliverPolicy } from 'nats'
+import { MikroORM } from '@mikro-orm/postgresql'
 
 import { connectJetstream, buildConsumer } from '../../core/src/nats'
 import { runIngestionLoop } from '../../core/src/ingestion'
 
-import mikroOrmConfig from "../mikro-orm.config"
+import mikroOrmConfig from '../mikro-orm.config'
 
 const main = (async () => {
-  const jetstreamConnection = await connectJetstream('nats://localhost:4222');
+  const jetstreamConnection = await connectJetstream('nats://localhost:4222')
 
   const consumer = await buildConsumer({
     connection: jetstreamConnection,
@@ -18,10 +18,10 @@ const main = (async () => {
       ack_policy: AckPolicy.All,
       deliver_policy: DeliverPolicy.All,
     },
-  });
+  })
 
   const orm = await MikroORM.init(mikroOrmConfig)
-  await orm.getMigrator().up();
+  await orm.getMigrator().up()
 
   await runIngestionLoop({ orm, consumer })
 })()
--- worker/src/reset.ts
+++ worker/src/reset.ts
@@ -1,15 +1,15 @@
 import fs from 'fs'
-import { MikroORM } from '@mikro-orm/postgresql';
+import { MikroORM } from '@mikro-orm/postgresql'
 
-import mikroOrmConfig from "../mikro-orm.config"
+import mikroOrmConfig from '../mikro-orm.config'
 
 const main = (async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
   await orm.em.getConnection().execute(`
     DO $$
     BEGIN
-      IF EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'bemi_local') THEN
-        PERFORM pg_drop_replication_slot('bemi_local');
+      IF EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'bemi_slot') THEN
+        PERFORM pg_drop_replication_slot('bemi_slot');
       END IF;
     END $$;
   `)
